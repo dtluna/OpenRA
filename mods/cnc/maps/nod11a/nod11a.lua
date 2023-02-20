85ea3d8969fb8321bc4c81fb081ff2374f422e11
dtluna <dtluna@riseup.net>
2023-02-20 04:19:27 +0400

    Add objectives

--- mods/cnc/maps/nod11a/nod11a.lua
+++ mods/cnc/maps/nod11a/nod11a.lua
@@ -15,6 +15,39 @@ else
 	Rambo = "rmbo"
 end
 
+NodBase = { NodConYard }
+
+ConYardDiscovered = function (_, discoverer)
+	if not Nod.IsObjectiveCompleted(FindBase) and discoverer == Nod then
+		Nod.MarkCompletedObjective(FindBase)
+
+		Utils.Do(NodBase, function (actor)
+			if not actor.IsDead then
+				actor.Owner = Nod
+			end
+		end)
+
+		Trigger.AfterDelay(DateTime.Seconds(1), function()
+			Media.PlaySpeechNotification(Nod, "NewOptions")
+		end)
+	end
+end
+
+TechCenterCaptured = function(_, _, _, newOwner)
+	if not Nod.IsObjectiveCompleted(CaptureTechCenter) and newOwner == Nod then
+		Nod.MarkCompletedObjective(CaptureTechCenter)
+
+		Trigger.AfterDelay(DateTime.Seconds(1), function()
+			Media.PlaySpeechNotification(Nod, "NewOptions")
+		end)
+	end
+end
+
+
+CheckForSams = function()
+	local sams = Nod.GetActorsByType("sam")
+	return #sams >= 3
+end
 
 WorldLoaded = function()
 	Nod = Player.GetPlayer("Nod")
@@ -27,14 +60,28 @@ WorldLoaded = function()
 	InitObjectives(Nod)
 
 	GDIObjective = GDI.AddPrimaryObjective("Eliminate all Nod forces in the area.")
-	WarFactoryObjective = Nod.AddPrimaryObjective("Destroy or capture the Weapons Factory.")
+
+	FindBase = Nod.AddPrimaryObjective("Find the Nod base.")
+	CaptureTechCenter = Nod.AddSecondaryObjective("Capture the Tech Center.")
+	BuildSAMs = Nod.AddSecondaryObjective("Build 3 SAMs to fend off the GDI bombers.")
+	EliminateGDI = Nod.AddPrimaryObjective("Eliminate all GDI forces in the area.")
 
 	Actor.Create(Rambo, true, { Owner = Nod, Location = RamboLocation.Location })
 
+	Trigger.OnDiscovered(NodConYard, ConYardDiscovered)
+	Trigger.OnCapture(TechCenter, TechCenterCaptured)
 end
 
 Tick = function()
 	if DateTime.GameTime > 2 and Nod.HasNoRequiredUnits() then
 		GDI.MarkCompletedObjective(GDIObjective)
+	end
+
+	if DateTime.GameTime > 2 and GDI.HasNoRequiredUnits() then
+		Nod.MarkCompletedObjective(EliminateGDI)
+	end
+
+	if not Nod.IsObjectiveCompleted(BuildSAMs) and CheckForSams() then
+		Nod.MarkCompletedObjective(BuildSAMs)
 	end
 end
